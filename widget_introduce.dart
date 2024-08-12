f576514ef446362c9d07b5daadaeeb66605ae48e
bjoongyoung <141486402+bjoongyoung@users.noreply.github.com>
2024-08-13 00:40:50 +0900

    widget의 종류 - (1)
    widget의 종류와 쓰임에 대해 알아보았다.

--- widget_introduce.dart
+++ widget_introduce.dart
@@ -27,7 +27,7 @@ class MyAPP extends StatelessWidget {
   }
 }
 
-//child와 children의 차이점//
+//widget의 종류//
 
 //child - 위젯을 하나만 추가할 수 있는 매개변수
 //child에서는 가운데로 정렬시키는 Center 위젯과 가로로 item들을 정렬하는 Row와 세로로 정렬하는 Column 등등의 위젯이 같이 사용된다.
