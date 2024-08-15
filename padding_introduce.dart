d7143529c3a7d26eb1e7f3fd00f7b3f1b5fb5de9
bjoongyoung <141486402+bjoongyoung@users.noreply.github.com>
2024-08-16 00:19:20 +0900

    padding 관련 Edgelnsets 클래스
    padding 관련 Edgelnsets 클래스에 대해 알아보고, all, symmetic, only, fromLTRM 들의 차이점과 어떻게 사용하는지 배워보았다.

---
+++ padding_introduce.dart
@@ -0,0 +1,19 @@
+//EdgeInsets Class
+
+/EdgeInsets.all(16)
+//상하좌우로 매개변수에 입력된 패딩을 균등하게 적용한다.
+
+
+/EdgeInsets.symmetric(vertical:16, horizontal: 16)
+//가로와 세로 패딩을 따로 적용한다.
+//Horizontal 매개변수에 가로로 적용할 패딩을 입력하고, Vertical 매개변수에 세로로 적용할 패딩을 입력한다
+
+
+/EdgeInsets.only(top:16, bottom:16, left:16, right:16)
+//위아래, 좌우 패딩을 따로 적용한다.
+//top, bottom, left, right 매개변수에 각각 위아래, 좌우 패딩을 입력할 수 있다.
+
+
+/EdgeInsets.fromLTRB(16, 16, 16, 16)
+//위아래, 좌우 패딩을 따로 적용한다.
+//포지셔널 파라미터를 좌,위,우.아래 순서로 입력한다.
