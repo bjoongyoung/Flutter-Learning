e3c364df3ee1ec282ad317864445b6b1542559d4
bjoonyoung <141486402+jsisodkd@users.noreply.github.com>
2024-08-17 01:16:43 +0900

    디자인 관련 위젯 - Container, SizedBox, Margin
    플러터에서 디자인할 때 많이 쓰이는 Container, SizedBox, Padding, Margin 들에 대해 알아보고 Padding과 Margin의 차이점을 새롭게 배웠다.

--- design-widget_introduce.dart
+++ design-widget_introduce.dart
@@ -32,4 +32,48 @@ SizedBox(
 
 
 //Padding Widget
+//child 위젯에서 여백을 줄 때 사용한다.
+//Padding 위젯을 사용하면 상위, 하위 위젯에 여백을 줄 수 있다.
+//Padding 위젯은 EdgeInsets 매개변수 값을 입력해야 한다.
+Container(
+    color: Colors.blue
+    child: Padding(
 
+        //상하, 좌우로 모두 16픽셀만큼 패딩 적용
+        Padding: EdgeInsets.all(16),
+        child: Container(
+            color: Colors.red,
+            width: 50,
+            height: 50,
+        ),
+    ),
+),
+
+
+//Margin Function
+//위젯의 바깥에 간격을 준다.
+//따로 위젯이 존재하지 않고 Container 위젯에 추가할 수 있다.
+//Margin과 Padding의 차이.
+Container(
+    color: Colors.black,
+
+    //중간 파란 컨테이너
+    child: Container(
+        color: Colors.blue,
+
+        //마진 적용 위치
+        margin: EdgeInsets.all(16),
+
+        //패딩 적용
+        child: Padding(
+            padding: EdgeInsets.all(16),
+
+            //패딩이 적용된 컨테이너
+            child: Container(
+                color: Colors.red,
+                width: 50,
+                height: 50,
+            ),
+        ),
+    ),
+),
