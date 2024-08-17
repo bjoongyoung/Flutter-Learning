671dce496d471975407ad07d82fe91e6d1bc64fe
bjoonyoung <141486402+jsisodkd@users.noreply.github.com>
2024-08-18 00:03:54 +0900

    디자인 관련 위젯 - MainAxisAlignment, CrossAxisAlignment
    플러터에서 디자인을 짤 때 가장 중요한 요소 중 하나인 MainAxisAlignment 와 CrossAxisAlignment 의 정렬 방법과 각각의 활용 방법을 알아보았다.

---
+++ alignment_introduce.dart
@@ -0,0 +1,45 @@
+//MainAxisAlignment
+//CrossAxisAlignment.center 기준
+
+/MainAxisAlignment.start
+//시작에 정렬
+
+
+/MainAxisAlignment.center
+//중앙에 정렬
+
+
+/MainAxisAlignment.end
+//끝에 정렬
+
+/MainAxisAlignment.spaceBetween
+//자식 위젯의 간격을 균등하게 정렬
+
+/MainAxisAlignment.spaceAround
+//자식 위젯의 간격을 균등하게 배정하고 왼쪽 끝과 
+//오른쪽 끝에 위젯 사이 거리의 반만큼 배정해 정렬
+
+
+/MainAxisAlignment.spaceEvenly
+//자식 위젯의 간격을 균등하게 배치하고 왼쪽 끝과
+//오른쪽 끝도 균등하게 배치
+
+
+
+//CrossAxisAlignment
+//MainAxisAlignment.center 기준
+
+/CrossAxisAlignment.start
+//시작에 정렬
+
+
+/CrossAxisAlignment.center
+//중앙에 정렬
+
+
+/CrossAxisAlignment.end
+//끝에 정렬
+
+
+/CrossAxisAlignment.stretch
+//반대축 최대한으로 늘려서 정렬
